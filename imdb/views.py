b51c612d981655a12935ab623e44fbf016384e63
Michael <michaelfhli@gmail.com>
2019-11-19 15:09:31 -0800

    show search criteria

--- imdb/views.py
+++ imdb/views.py
@@ -37,10 +37,14 @@ def search(request, page=1):
             num_votes__gte=min_votes).filter(num_votes__lte=max_votes).filter(
             runtime__gte=min_runtime).filter(runtime__lte=max_runtime)
 
+        criteria = f'{results.count()} Matches. Results for titles: "{title}" Release Date: {min_year} - {max_year} ' \
+                   f"Ranking: {min_rank} - {max_rank} Rating: {min_rating} - {max_rating} " \
+                   f"Votes: {min_votes} - {max_votes}"
+
         context = {'num_results': results.count(), 'min_year': min_year, 'max-year': max_year, 'min_rank': min_rank,
                    'max-rank': max_rank, 'min_rating': min_rating, 'max-rating': max_rating, 'min_votes': min_votes,
                    'max-votes': max_votes, 'min_runtime': min_runtime, 'max-runtime': max_runtime, 'results': results,
-                   }
+                   'criteria': criteria}
         return render(request, 'search.html', context=context)
     else:
         return render(request, 'home.html')
